71986d8a04dcc02121cae62a6ac3ab4042bb55b0
jaivikthakkar <jaivikthakkar2@gmail.com>
2026-08-10 19:34:18 +0530

    newlogic

---
+++ prectic_hostel/star_prnting/squer_h.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j;
+    for(int i=1;i<=2;i++){
+        for(int j=1;j<=2;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
