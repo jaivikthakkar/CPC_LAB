e120fd68d81676b8323abefb9198303a188009ba
jaivikthakkar <jaivikthakkar2@gmail.com>
2026-09-24 15:41:10 +0530

    Implement matrix input and transpose display functionality

--- lab/16_lab/09_lab.c
+++ lab/16_lab/09_lab.c
@@ -2,6 +2,31 @@
 
 int main()
 {
-    
+    int m, n;
+    printf("enter m and n ");
+    scanf("%d", &m);
+    int array[m][m];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (i<j)
+            {
+                array[i][j]=0;
+            }
+            else
+            {
+                 scanf("%d", &array[i][j]);
+            }
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d ", array[j][i]);
+        }
+        printf("\n");
+    }
     return 0;
 }
